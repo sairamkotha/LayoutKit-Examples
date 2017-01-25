4674bbf670b747972db5ec46bb7311a7d4f7a240
SaiRam <sairamkotha007@gmail.com>
2017-01-25 20:34:46 +0530

    adding .gitignore

---
+++ LayoutKitExamples/ExampleLayouts/TopDividerStackLayout.swift
@@ -0,0 +1,13 @@
+//
+//  TopDividerStackLayout.swift
+//  LayoutKitExamples
+//
+//  Created by SaiRam on 25/01/17.
+//  Copyright © 2017 Sairamkotha. All rights reserved.
+//
+
+import UIKit
+
+class TopDividerStackLayout: NSObject {
+
+}
